f513e9fd6aba8abdf52bd92a82cde15c05a73fa0
Lee Garam <agerio100@naver.com>
2024-01-07 15:29:40 +0900

    Finish multimodal implementation

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ import (
 
 // main testing
 func main() {
+	// modelTextInputTextOutput()
+	modelMultimoalInputTextOutput()
+}
+
+// An example usage of
+// - input: text only query
+// - output: text only response
+func modelTextInputTextOutput() {
 	// Prepare the model
 	modelName := "gemini-pro"
 	if _, error := modelUsages.ModelConnectionTest(modelName); error != nil {
@@ -20,6 +28,25 @@ func main() {
 	// ask a question and receive an answer of it
 	question := "Give me any famous quote from popular people"
 	response := modelUsages.GenerateTextFromTextOnlyInput(modelName, question)
+
+	// Procces the data to beautify UwU
+	result, _ := responseProcess.GetGeminiAITextOnlyResponseStruct(question, response)
+	resultInJSON, _ := json.MarshalIndent(result, "", "	")
+	fmt.Println(string(resultInJSON))
+
+}
+
+// An example usage of
+// - input: multimodal input(text + image(s))
+// - output: text only response
+func modelMultimoalInputTextOutput() {
+	// model set is fixed to "gemini-pro-vision" automatically
+	question := "Describe about the first and second images related to desert"
+	imagePathSlice := []string{
+		"modules/modelUsages/sampleImages/cheesecake.jpeg",
+		"modules/modelUsages/sampleImages/coffee.png",
+	}
+	response := modelUsages.GenerateTextFromMultimodalInput(question, imagePathSlice)
 
 	// Procces the data to beautify UwU
 	result, _ := responseProcess.GetGeminiAITextOnlyResponseStruct(question, response)
--- modules/modelUsages/generateTextFromMultimodalInput.go
+++ modules/modelUsages/generateTextFromMultimodalInput.go
@@ -2,9 +2,12 @@ package modelUsages
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"main/modules/apikey"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
@@ -24,13 +27,32 @@ func GenerateTextFromMultimodalInput(question string, inputImagePathArray []stri
 	defer client.Close()
 
 	model := client.GenerativeModel("gemini-pro-vision")
-	prompt := make([]genai.Part, len(inputImagePathArray)+1)
+	prompt := []genai.Part{}
 	for _, imagePath := range inputImagePathArray {
+		// Trying to open images
 		imageData, error := os.ReadFile(imagePath)
 		if error != nil {
 			log.Panic(error)
 		}
-		prompt = append(prompt, genai.ImageData("jpeg", imageData))
+
+		// Valid extension check. "/path/fo/file.png" => "png" (Even remove dot(.))
+		imageFileExtension := strings.ToLower(filepath.Ext(imagePath)[1:])
+		if !(imageFileExtension == "png" ||
+			imageFileExtension == "jpg" ||
+			imageFileExtension == "jpeg" ||
+			imageFileExtension == "webp" ||
+			imageFileExtension == "heic" ||
+			imageFileExtension == "heif") {
+			errorMessage := fmt.Sprintf("The allowed file formats are only png, jp(e)g, webp, heic, heif, but received %s extension\n", imageFileExtension)
+			// Actually, JPG is the same with JPEG. In official docs of Gemini API,
+			// jpeg is only supported. (However, JPEG is also acceptable, so just consider JPG as JPEG)
+			if imageFileExtension == "jpg" {
+				imageFileExtension = "jpeg"
+			}
+			log.Panic(errorMessage)
+		}
+		imageDataPart := genai.ImageData(imageFileExtension, imageData)
+		prompt = append(prompt, imageDataPart)
 	}
 	prompt = append(prompt, genai.Text(question))
 
